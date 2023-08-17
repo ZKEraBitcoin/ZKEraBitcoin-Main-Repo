e0494edd85db0bf447731a1bdcc5057b1dbee1d5
ZKEraBitcoin <142457573+ZKEraBitcoin@users.noreply.github.com>
2023-08-17 13:35:57 -0500

    Update EraBitcon.sol

--- solidity/EraBitcon.sol
+++ solidity/EraBitcon.sol
@@ -202,6 +202,7 @@ contract eraBitcoin is IERC20 {
 		_startNewMiningEpoch();
 
 		balances[msg.sender] = balances[msg.sender].add(reward_amount);
+		emit Transfer(msg.sender, reward_amount);
 		tokensMinted = tokensMinted.add(reward_amount);
 
 		emit Mint(msg.sender, reward_amount, epochCount, challengeNumber );
